1ab51aebabe4be2d9d054e0b2bb3fee2a0f0e67d
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-05 17:21:14 -0300

    Startup.cs - Adição de comentários para estudo

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -28,7 +28,7 @@ namespace SalesWebMvc
 
         public IConfiguration Configuration { get; }
 
-        // Este método é chamado pelo tempo de execução. 
+        // Este método é chamado pelo tempo de execução.
         // Use este método para adicionar serviços ao contêiner.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -39,22 +39,22 @@ namespace SalesWebMvc
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            // Define o Microsoft.AspNetCore.Mvc.CompatibilityVersion para ASP.NET Core MVC para o aplicativo.
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            // Definição do Banco de dados
             services.AddDbContext<SalesWebMvcContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
-builder.MigrationsAssembly("SalesWebMvc")));
+                        builder.MigrationsAssembly("SalesWebMvc")));
 
             // Registro dos Services no Container
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
             services.AddScoped<DepartmentService>();
             services.AddScoped<SalesRecordService>();
-
         }
 
-        // Este método é chamado pelo tempo de execução. 
+        // Este método é chamado pelo tempo de execução.
         // Use este método para configurar o pipeline de solicitação HTTP.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
@@ -66,23 +66,39 @@ builder.MigrationsAssembly("SalesWebMvc")));
                 SupportedUICultures = new List<CultureInfo> { enUS }
             };
 
+            // Definir automaticamente as informações de cultura para solicitações com base nas informações fornecidas pelo cliente.
             app.UseRequestLocalization(localizationOptions);
 
+            // Verifica qual ambiente o App se encontra
             if (env.IsDevelopment())
             {
+                // Middleware para capturar instâncias System.Exception síncronas e assíncronas do pipeline e gera respostas de erro HTML
                 app.UseDeveloperExceptionPage();
+
+                // Popula o banco de dados default
                 seedingService.Seed();
             }
             else
             {
+                // Middleware que captura exceções, registra-as, redefine o caminho da solicitação e reexecuta a solicitação.
+                // A solicitação não será executada novamente se a resposta já tiver começado.
                 app.UseExceptionHandler("/Home/Error");
+
+                // Middleware para usar HSTS, que adiciona o cabeçalho Strict-Transport-Security.
                 app.UseHsts();
             }
 
+            // Middleware para redirecionar solicitações HTTP para HTTPS.
             app.UseHttpsRedirection();
+
+            // Middleware para ativar o envio de arquivo estático para o caminho da solicitação atual.
+            // Permite que os arquivos estáticos sejam atendidos
             app.UseStaticFiles();
+
+            // Middleware para ativa os recursos de política de cookies
             app.UseCookiePolicy();
 
+            // Adiciona MVC ao pipeline de execução de solicitação Microsoft.AspNetCore.Builder.IApplicationBuilder
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
